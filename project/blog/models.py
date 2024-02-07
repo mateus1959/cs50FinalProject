5c8a05682262657ceaa011980bd45dfeba9d0341
Mateus Machado <mateus.machado@saintleo.edu>
2024-02-07 12:18:56 -0500

    Update project

--- project/blog/models.py
+++ project/blog/models.py
@@ -7,8 +7,14 @@ class Book(models.Model):
     genre = models.CharField(max_length=50)
     likes = models.IntegerField(default=0) # How many people endorse that book.
 
+    def __str__(self):
+        return f"\"{self.title}\""
+
 
 class Post(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE) # Foreign key to hold the book ID of the book this post is about
     name = models.CharField(max_length=50) # Username of the user who read the book
     date = models.DateField("date posted") # Date on which user posted
+
+    def __str__(self):
+        return f"{self.date} - {self.name}: {self.book}"
--- project/blog/views.py
+++ project/blog/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import * 
+import datetime
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This is the index view")
+    posts = Post.objects.all()
+    return render(request, "blog/index.html", {
+        "posts": posts
+    })
