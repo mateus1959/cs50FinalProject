917911919f8cad67e020188732c932ddfc0e660a
Mateus Machado <mateus.machado@saintleo.edu>
2024-02-07 11:04:00 -0500

    Create blog app

--- project/calculator/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.template import loader
-
-# Create your views here.
-def index(request):
-    template = loader.get_template("calculator/index.html")
-    context = {}
-    return HttpResponse(template.render(context, request))
