22ef8eb18f702ec5e63f3eeb5b74b5adba94b46d
Mateus Machado <mateus.machado@saintleo.edu>
2024-02-07 11:22:18 -0500

    Update blog/models.py

--- project/blog/models.py
+++ project/blog/models.py
@@ -2,7 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200) 
     author = models.CharField(max_length=100)
     genre = models.CharField(max_length=50)
-    likes = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0) # How many people endorse that book.
+
+
+class Post(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE) # Foreign key to hold the book ID of the book this post is about
+    name = models.CharField(max_length=50) # Username of the user who read the book
+    date = models.DateField("date posted") # Date on which user posted
